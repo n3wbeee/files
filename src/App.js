eb37e136ddd76cbf7b79db85b58c8059d772254f
XD <1063787103@qq.com>
2022-11-07 19:07:40 +0800

    组件: ButtonCounter

--- src/App.js
+++ src/App.js
@@ -3,29 +3,18 @@
 import React from "react";
 
 class TestComponent extends React.Component {
-	//定义组件状态
+	// 通过state定义组件状态
 	state = {
-		name: "test",
-	};
-	//修改组件状态
-	changeStateName = () => {
-		if (this.state.name === "test") {
-			this.setState({
-				name: "test1",
-			});
-		} else {
-			this.setState({
-				name: "test",
-			});
-		}
+		counter: 0,
 	};
 	render() {
-		//使用组件状态
 		return (
-			<div>
-				This Is Test Component 当前状态为: {this.state.name}
-				<button onClick={this.changeStateName}>修改状态</button>
-			</div>
+			<button
+				onClick={() => {
+					this.setState({ counter: this.state.counter + 1 });
+				}}>
+				{this.state.counter}Click
+			</button>
 		);
 	}
 }
