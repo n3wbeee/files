a93b66c079d18b13ca1fc2806e67e232cb859aea
XD <1063787103@qq.com>
2022-11-08 12:11:24 +0800

    组件: 组件通信--子传父

--- src/App.js
+++ src/App.js
@@ -2,38 +2,22 @@
 
 import React from "react";
 
-function SonFunc({ message, sonFunc, childSpan }) {
-	//通过解构赋值获取父组件传递的参数
-	//props内存放的是父组件传递过来的数据
-	//!!!注意：props是只读的，不能修改
-	return (
-		<div>
-			测试--函数组件,{message}
-			<button onClick={sonFunc}>调用父组件的方法</button>
-			{childSpan}
-		</div>
-	);
+function SonFunc({ getSonMessage }) {
+	return <button onClick={() => getSonMessage("来自子组件")}>子组件</button>; //子组件向父组件传值
 }
 
 class App extends React.Component {
-	state = {
-		message: "hello world",
+	getSonMessage = (message) => {
+		console.log(message);
 	};
-	FatherFunc = () => {
-		console.log("调用父组件的方法");
-	};
+
 	render() {
 		return (
-			<div>
-				<SonFunc
-					message={this.state.message}
-					sonFunc={this.FatherFunc}
-					childSpan={<span>我是span</span>}
-				/>
-			</div>
+			<>
+				<SonFunc getSonMessage={this.getSonMessage} />
+			</>
 		);
 	}
 }
 
 export default App;
-
