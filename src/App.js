afb12b10036bd16fbd497b32d34955f209e689cf
XD <1063787103@qq.com>
2022-11-08 12:00:28 +0800

    组件: 组件通信--props解构赋值

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@
 
 import React from "react";
 
-function SonFunc(props) {
+function SonFunc({ message, sonFunc, childSpan }) {
+	//通过解构赋值获取父组件传递的参数
 	//props内存放的是父组件传递过来的数据
 	//!!!注意：props是只读的，不能修改
 	return (
 		<div>
-			测试--函数组件,{props.message}
-			<button onClick={props.sonFunc}>调用父组件的方法</button>
-			{props.childSpan}
+			测试--函数组件,{message}
+			<button onClick={sonFunc}>调用父组件的方法</button>
+			{childSpan}
 		</div>
 	);
 }
