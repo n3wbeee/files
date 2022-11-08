e78920ca9663382555781aeaacd09337500bc8ed
XD <1063787103@qq.com>
2022-11-08 11:56:07 +0800

    组件: 组件通信--传递函数、JSX

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@
 
 import React from "react";
 
-//子组件（函数）
 function SonFunc(props) {
 	//props内存放的是父组件传递过来的数据
 	//!!!注意：props是只读的，不能修改
-	return <div>测试--函数组件,{props.message}</div>;
-}
-
-//子组件（类）
-class SonClass extends React.Component {
-	render() {
-		return <div>测试--类组件,{this.props.message}</div>;
-	}
+	return (
+		<div>
+			测试--函数组件,{props.message}
+			<button onClick={props.sonFunc}>调用父组件的方法</button>
+			{props.childSpan}
+		</div>
+	);
 }
 
 class App extends React.Component {
 	state = {
 		message: "hello world",
 	};
+	FatherFunc = () => {
+		console.log("调用父组件的方法");
+	};
 	render() {
 		return (
 			<div>
-				<SonFunc message={this.state.message} />
-				<SonClass message={this.state.message} />
+				<SonFunc
+					message={this.state.message}
+					sonFunc={this.FatherFunc}
+					childSpan={<span>我是span</span>}
+				/>
 			</div>
 		);
 	}
