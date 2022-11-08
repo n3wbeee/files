cc36a16fe31061c127082f35fc60430d420775fd
XD <1063787103@qq.com>
2022-11-08 16:42:26 +0800

    组件: 组件通信--练习

--- src/App.js
+++ src/App.js
@@ -2,35 +2,56 @@
 
 import React, { createContext } from "react";
 
-const { Provider, Consumer } = createContext();
-
-function ComA() {
+function ListItem({ item, delFunc }) {
 	return (
-		<div>
-			this is ComA
-			<ComC />
-		</div>
-	);
-}
-
-function ComC() {
-	return (
-		<div>
-			<Consumer>{(value) => <span>{value}</span>}</Consumer>
-			ComC
-		</div>
+		<>
+			<h3>{item.name}</h3>
+			<p>{item.info}</p>
+			<p>{item.price}</p>
+			<button onClick={() => delFunc(item.id)}>删除</button>
+		</>
 	);
 }
 
 class App extends React.Component {
 	state = {
-		message: "hello world",
+		list: [
+			{
+				id: 1,
+				name: "超级好吃的棒棒糖",
+				price: 18.8,
+				info: "开业大酬宾，全场8折",
+			},
+			{
+				id: 2,
+				name: "超级好吃的大鸡腿",
+				price: 34.2,
+				info: "开业大酬宾，全场8折",
+			},
+			{
+				id: 3,
+				name: "超级无敌的冰激凌",
+				price: 14.2,
+				info: "开业大酬宾，全场8折",
+			},
+		],
+	};
+	deleteListItem = (id) => {
+		this.setState({
+			list: this.state.list.filter((item) => item.id !== id),
+		});
 	};
 	render() {
 		return (
-			<Provider value={this.state.message}>
-				<ComA />
-			</Provider>
+			<>
+				{this.state.list.map((item) => (
+					<ListItem
+						key={item.id}
+						item={item}
+						delFunc={this.deleteListItem}
+					/>
+				))}
+			</>
 		);
 	}
 }
