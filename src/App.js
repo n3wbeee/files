385411f4573b9b3d41517050a38b792ea03ab7f1
XD <1063787103@qq.com>
2022-11-09 11:13:55 +0800

    Hook: useState

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 /** @format */
 
 import React from "react";
-import PropTypes from "prop-types";
+import { useState } from "react";
 
-function Test({ list }) {
+function App() {
+	const [count, setCount] = useState(0);
+
 	return (
-		<div>
-			{/* 也许传入的list不是数组...?*/}
-			{list.map((item) => (
-				<p>{item}</p>
-			))}
-		</div>
+		<>
+			<button onClick={() => setCount(count + 1)}>{count}</button>
+		</>
 	);
-}
-Test.propTypes = {
-	list: PropTypes.array, // 限定list的类型为数组
-};
-
-class App extends React.Component {
-	render() {
-		return (
-			<>
-				<Test list={[1, 2, 3]} />
-			</>
-		);
-	}
 }
 
 export default App;
